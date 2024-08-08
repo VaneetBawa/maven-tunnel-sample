0a4e2ac193c9a49a8de9483b4d2a62ba16ce6176
VaneetBawa <37815424+VaneetBawa@users.noreply.github.com>
2024-08-08 20:51:47 +0530

    Update MavenSingle.java

--- src/test/java/com/lambdatest/tunnel/MavenSingle.java
+++ src/test/java/com/lambdatest/tunnel/MavenSingle.java
@@ -38,14 +38,15 @@ public class MavenSingle implements IExecutionListener {
 
     System.out.println("name is" + capabilities);
     // create tunnel instance
-    //t = new Tunnel();
-    // HashMap<String, String> options = new HashMap<String, String>();
-    // options.put("user", username);
-    // options.put("key", access_key);
-    // options.put("tunnelName", "MavenSingle");
+    t = new Tunnel();
+    HashMap<String, String> options = new HashMap<String, String>();
+    options.put("user", username);
+    options.put("logFile", "src/test");
+    options.put("key", access_key);
+    options.put("tunnelName", "MavenSingle");
 
-    // // start tunnel
-    // t.start(options);
+    // start tunnel
+    t.start(options);
     driver = new RemoteWebDriver(new URL("http://" + username + ":" + access_key + "@hub.lambdatest.com/wd/hub"),
         capabilities);
     System.out.println("Started session");
@@ -64,6 +65,6 @@ public class MavenSingle implements IExecutionListener {
     ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
     driver.quit();
     // close tunnel
-    //t.stop();
+    t.stop();
   }
 }
