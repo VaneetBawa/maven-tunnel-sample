ce804b18ef645f77ddd4069610a43a23c1955418
VaneetBawa <37815424+VaneetBawa@users.noreply.github.com>
2025-02-06 00:28:05 +0530

    Update MavenSingle.java

--- src/test/java/com/lambdatest/tunnel/MavenSingle.java
+++ src/test/java/com/lambdatest/tunnel/MavenSingle.java
@@ -38,15 +38,15 @@ public class MavenSingle implements IExecutionListener {
 
     System.out.println("name is" + capabilities);
     // create tunnel instance
-    // t = new Tunnel();
-    // HashMap<String, String> options = new HashMap<String, String>();
-    // options.put("user", username);
-    // options.put("logFile", "./");
-    // options.put("key", access_key);
-    // options.put("tunnelName", "MavenSingle");
+    t = new Tunnel();
+    HashMap<String, String> options = new HashMap<String, String>();
+    options.put("user", username);
+    options.put("logFile", "./");
+    options.put("key", access_key);
+  //  options.put("tunnelName", "MavenSingle");
 
-    // // start tunnel
-    // t.start(options);
+    // start tunnel
+    t.start(options);
     driver = new RemoteWebDriver(new URL("http://" + username + ":" + access_key + "@hub.lambdatest.com/wd/hub"),
         capabilities);
     System.out.println("Started session");
@@ -55,7 +55,7 @@ public class MavenSingle implements IExecutionListener {
   @Test()
   public void testTunnel() throws Exception {
     // Check LocalHost on XAMPP
-    driver.get("http://localhost.lambdatest.com");
+   // driver.get("http://ww.lambdatest.com");
     // Let's check that the item we added is added in the list.
     driver.get("https://google.com");
   }
@@ -65,6 +65,6 @@ public class MavenSingle implements IExecutionListener {
     ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
     driver.quit();
     // close tunnel
-  //  t.stop();
+    t.stop();
   }
 }
